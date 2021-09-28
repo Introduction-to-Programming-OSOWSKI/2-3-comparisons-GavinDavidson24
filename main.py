ea6267fd44edc87cce0a56960500ddba02f33ebe
GavinDavidson24 <818399@mystma.org>
2021-09-28 14:48:36 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+def lessThan(x, y):
+    if x < y:
+        return True
+def equalTo(x, y):
+    if x == y:
+        return True
+def greaterOrEqual(x, y):
+    if x > y:
+        return True
+    elif x == y:
+        return True
+def lessOrEqual(x, y):
+    if x < y:
+        return True
+    elif x == y:
+        return True
